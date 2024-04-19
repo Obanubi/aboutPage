2fbfab39fa021a14ab61e08de3fe766ba67a9de4
Obanubi <153923809+Obanubi@users.noreply.github.com>
2024-04-19 16:35:29 +0100

    Add files via upload

---
+++ About_page/src/components/Text.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Text.css'
+const Text = () => {
+    return (
+
+        <div className='all'>
+            <h1>About Travel Experts</h1> <br />
+
+            <div>
+                <h3>Travel Experts has been a premier travel design firm since 1989, made up of only the industry’s premium travel counselors. Travel Experts members are experienced professionals who operate as independent contractors, fully embracing the entrepreneurial spirit of their individual businesses. They have the travel expertise and business acumen needed to enable them to work independently while at the same time enjoying the advantages of associating with a nationally recognized premier agency.
+
+                What this means to you, the traveler is the best possible service and attention because it is in their best interest to orchestrate an amazing trip for you. Through our membership in Virtuoso, an invitation only network of the world's best travel agencies, we can offer you our personal relationships with the top hotels, resorts, spas, lodges, airline, cruise lines and tour companies to provide you with incredible values and rare experiences - from complimentary upgrades and amenities to special recognition and privileged access.
+
+                As you browse through our travel consultants, you will note an extensive variety of specialists who can satisfy all types of travel needs including vacation planning, family adventure, group and incentive travel, religious travel and corporate travel management. We invite you to search out the specialist that will create the perfect trip.</h3> <br/>
+            </div>
+            <div>
+                    <h1>Our Management Team</h1>
+                <img src="./src/assets/abtpic.jpeg" alt="" width={980} />
+            </div>  <br/>
+
+                 <div className='position'> 
+                 div
+                    <h4>Susan Ferrell</h4> <br/>
+                     <h5>President & Owner</h5> <br/>
+                    <a href="mailto:">Email Susan</a>
+
+                     <h4>Susan Ferrell</h4>
+                    <h5>President & Owner</h5>
+                    <a href="mailto:">Email Susan</a>
+
+                    <h4>Susan Ferrell</h4>
+                    <h5>President & Owner</h5>
+                    <a href="mailto:">Email Susan</a>
+
+                    <h4>Susan Ferrell</h4>
+                    <h5>President & Owner</h5>
+                    <a href="mailto:">Email Susan</a>  
+
+                 </div>    
+
+        </div>
+        
+    )
+}
+
+export default Text
